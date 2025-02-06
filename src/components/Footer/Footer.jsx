5fd4f525a46733f0dcddea0fc7fea4af762deab6
JhonyBe77 <jjruiperezf@gmail.com>
2025-02-06 12:49:00 +0100

    addFooter

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <footer>
+    <p>Juan José Ruipérez Fernández · Especialista en Diseño Gráfico | Desarrollador Web Full Stack</p>
+    <section>
+      <article>
+        <a href="mailto:jjruiperezf@gmail.com">Contacto</a>
+      </article>
+      <article>
+        <a href="https://github.com/JhonyBe77" target="_blank"><FaGithub className="footerIcon" /></a>
+        <a href="https://www.linkedin.com/in/jjruiperez/" target="_blank"><FaLinkedin className="footerIcon" /></a>
+      </article>
+    </section>
+  </footer>;
 };
 
 export default Footer;
