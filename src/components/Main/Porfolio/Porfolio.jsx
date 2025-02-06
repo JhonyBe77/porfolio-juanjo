645c1b9ae439a5a8ff4280f2cc7fdf0e09913034
JhonyBe77 <jjruiperezf@gmail.com>
2025-02-06 21:29:34 +0100

    addImagesWorks

--- src/components/Main/Porfolio/Porfolio.jsx
+++ src/components/Main/Porfolio/Porfolio.jsx
@@ -3,8 +3,8 @@ import { context } from "../../../context/context";
 
 import imgOptica from "../../../assets/porfolio_imgs/appOptica.png";
 import imgPixelist from "../../../assets/porfolio_imgs/optica.png";
-import imgChatbot from "../../../assets/porfolio_imgs/appOptica.png";
-import imgJobOffers from "../../../assets/porfolio_imgs/Quiz.png";
+import imgChatbot from "../../../assets/porfolio_imgs/felgtbi.png";
+import imgConectic from "../../../assets/porfolio_imgs/conectic4.png";
 
 import { MdOpenInNew } from "react-icons/md";
 import techLogos from "./techLogos";
@@ -13,8 +13,8 @@ const projectsData = [
   {
     id: "optica",
     title: "APP Óptica O-Q",
-    description: "Web para filtrar juegos por categorías y ponerlos en favoritos. API externa para mostrar información y API propia para gestionar usuarios.",
-    details: "Proyecto personal individual para Bootcamp Fullstack.",
+    description: "Web para ópticos y clientes, con navegación intuitiva, gestión de favoritos y diseño responsivo.",
+    details: "Bootcamp Fullstack. Diseño y desarrollo de interfaz y código realizados con:",
     features: ["Categorías", "Scraping", "Buscador", "API externa", "Sistema de login", "Sistema de favoritos", "UI/UX"],
     img: imgOptica,
     link: "https://pixelist.onrender.com/",
@@ -24,8 +24,8 @@ const projectsData = [
     id: "chatbot",
     title: "CHATBOT FELGTBIQ",
     description: "Chatbot intuitivo y accesible que ofrece atención y orientación sobre el VIH, además de estadísticas anonimizadas de las respuestas.",
-    details: "Realizado junto con Data Science + Ciberseguridad.",
-    features: ["Sistema de formularios", "Sistema de login", "Librería de Chatbot", "UI/UX", "Cliente real"],
+    details: "Bootcamp Fullstack. Diseño y desarrollo de interfaz y código realizados con:",
+    features: ["Chatbot Real","Formularios con Chatbot", "DoList", "Login", "UI/UX"],
     img: imgChatbot,
     link: "https://chatbot-felgtbiq-front.onrender.com/",
     tech: ["react", "sass", "node", "express", "postgresql"],
@@ -36,40 +36,11 @@ const projectsData = [
     description: "Web para buscar trabajos como freelance mediante scraping. Rol admin para crear ofertas adicionales.",
     details: "Trabajo en equipo para Bootcamp Fullstack.",
     features: ["Sistema de login + Google", "Scraping", "Sistema de favoritos", "UI/UX", "Buscador"],
-    img: imgJobOffers,
+    img: imgConectic,
     link: "https://backendfinalproject-nbhl.onrender.com/",
     tech: ["pug", "css", "node", "express", "postgresql"],
   },
-  {
-    id: "pixelist",
-    title: "APP Óptica O-Q",
-    description: "Web para filtrar juegos por categorías y ponerlos en favoritos. API externa para mostrar información y API propia para gestionar usuarios.",
-    details: "Proyecto personal individual para Bootcamp Fullstack.",
-    features: ["Sistema de login", "Sistema de favoritos", "API externa", "UI/UX", "Buscador"],
-    img: imgPixelist,
-    link: "https://pixelist.onrender.com/",
-    tech: ["react", "sass", "node", "express", "postgresql"],
-  },
-  {
-    id: "chatbot",
-    title: "CHATBOT FELGTBIQ",
-    description: "Chatbot intuitivo y accesible que ofrece atención y orientación sobre el VIH, además de estadísticas anonimizadas de las respuestas.",
-    details: "Realizado junto con Data Science + Ciberseguridad.",
-    features: ["Sistema de formularios", "Sistema de login", "Librería de Chatbot", "UI/UX", "Cliente real"],
-    img: imgChatbot,
-    link: "https://chatbot-felgtbiq-front.onrender.com/",
-    tech: ["react", "sass", "node", "express", "postgresql"],
-  },
-  {
-    id: "jobOffers",
-    title: "FREELANCE JOB OFFERS",
-    description: "Web para buscar trabajos como freelance mediante scraping. Rol admin para crear ofertas adicionales.",
-    details: "Trabajo en equipo para Bootcamp Fullstack.",
-    features: ["Sistema de login + Google", "Scraping", "Sistema de favoritos", "UI/UX", "Buscador"],
-    img: imgJobOffers,
-    link: "https://backendfinalproject-nbhl.onrender.com/",
-    tech: ["pug", "css", "node", "express", "postgresql"],
-  },
+
 ];
 
 const Porfolio = () => {
@@ -83,8 +54,10 @@ const Porfolio = () => {
           <article key={id} className="projectCard">
             <img src={img} alt={`Captura de ${title}`} />
             <div className="cardContent">
+              <h4>{title}</h4>
+              <p>{description}</p>
+              <p>{details}</p>
               <div className="titleLogos">
-                <h4>{title}</h4>
                 <ul>
                   {tech.map((techName) => (
                     <li key={techName}>
@@ -97,18 +70,16 @@ const Porfolio = () => {
                   ))}
                 </ul>
               </div>
-              <p>{description}</p>
-              <p>{details}</p>
               <ul className="listInfo">
                 {features.map((feature, index) => (
                   <li key={index}>{feature}</li>
                 ))}
               </ul>
-              <a href={link} target="_blank" rel="noopener noreferrer">
+              {/* <a href={link} target="_blank" rel="noopener noreferrer">
                 <button>
                   Ver online <MdOpenInNew />
                 </button>
-              </a>
+              </a> */}
             </div>
           </article>
         ))}
